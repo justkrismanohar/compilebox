97f115481d631d83e1085e31148460d16e3aaa98
root <justkrismanohar@gmail.com>
2017-10-14 09:24:35 +0000

    Fixed install issues

---
+++ Setup/startDocker.sh
@@ -0,0 +1,8 @@
+###########################
+# Start Docker
+###########################
+chmod 777 ../API/DockerTimeout.sh
+chmod 777 ../API/Payload/script.sh
+chmod 777 ../API/Payload/javaRunner.sh
+chmod 777 UpdateDocker.sh
+echo "Permissions Changed"
