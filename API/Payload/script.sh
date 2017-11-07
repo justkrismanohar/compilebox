0119beb0a6d569f8ee605dbc3b207c3ea4c89c02
Ubuntu <ubuntu@ip-172-31-37-19.us-east-2.compute.internal>
2017-11-07 06:13:27 +0000

    Added support for files

--- API/Payload/script.sh
+++ API/Payload/script.sh
@@ -41,45 +41,76 @@ exec  1> $"/usercode/stdout.txt"
 exec  2> $"/usercode/errors"
 #3>&1 4>&2 >
 
-
-type=$(sed -n 1p /usercode/config)
+#read first line of config, remove trailing return character
+type=$(sed -n 1p /usercode/config | tr -d '\r')
 testStart=1
-testEnd=$(sed -n 2p /usercode/config)
+#read second line of config, remove trailing return character
+testEnd=$(sed -n 2p /usercode/config | tr -d '\r')
 
 
 START=$(date +%s.%2N)
-#Branch 1
-if [ "$output" = "" ]; then
-    $compiler /usercode/$file -< $"/usercode/inputFile" #| tee /usercode/output.txt
-#Branch 2
-else
-	#In case of compile errors, redirect them to a file
-        $compiler /usercode/$file $addtionalArg #&> /usercode/errors.txt
-	#Branch 2a
-	if [ $? -eq 0 ];	then
-		for ((i=testStart; i<=$testEnd; i++))
-		do 
-			echo "test$i.in"
+#Maybe loop in an array or something later...
+if [ $type -eq 10 ] || [ $type -eq 11 ] || [ $type -eq 12 ] || [ $type -eq 13 ]; then	
+    #type 10 read from stdin and write to stdout
+	#type 11 reads from input.txt writes to output.txt
+	#type 12 reads from sdtin writes to output.txt
+	#type 13 reads from input.txt and writes to stdout
 
-			$output -< $"/usercode/test$i.in" | tee "/usercode/output.txt"
-			
-			if diff -Z "/usercode/output.txt" "/usercode/test$i.out" > /dev/null
-			 then
-				echo "*-COMPILEBOX::DECISION-*,Accepted,test$i" >> "/usercode/logfile.txt"
-			 else
-				echo "*-COMPILEBOX::DECISION-*,Wrong Answer,test$i" >> "/usercode/logfile.txt"
-				break
-			fi
-
-		done    
-	#Branch 2b
+	#Branch 1
+	if [ "$output" = "" ]; then
+		$compiler /usercode/$file -< $"/usercode/inputFile" #| tee /usercode/output.txt
+	#Branch 2
 	else
-	    echo "Compilation Failed" >> "usercode/logfile.txt"
-	    #if compilation fails, display the output file	
-	    #cat /usercode/errors.txt
-	fi
-fi
+		#In case of compile errors, redirect them to a file
+			$compiler /usercode/$file $addtionalArg #&> /usercode/errors.txt
+		#Branch 2a
+		if [ $? -eq 0 ];	then
+			#User's code write to local dir, docker logs into root dir
+			#by changing directory ensures the expected output.txt is written
+			#to the /usercode folder			
+			cd "/usercode"
+			
+			for ((i=testStart; i<=$testEnd; i++))
+			do 
+				echo "test$i.in"
+				
+				if [ $type -eq 11 ] || [ $type -eq 13 ]; then 
+					cp -f "/usercode/test$i.in" "/usercode/input.txt"
+				fi
+				
+				#user code expected to create an output.txt file
+				if [ $type -eq 10 ]; then	
+					$output -< $"/usercode/test$i.in" | tee "/usercode/output.txt"
+				elif [ $type -eq 11 ]; then 
+					$output
+				elif [ $type -eq 12 ]; then 
+					$output -< $"/usercode/test$i.in"
+				elif [ $type -eq 13 ]; then
+					#change output.txt to something obsure, so user can't guess
+					#what it is...worst case, by writing to output.txt in this case
+					#they will messup their own output....
+					$output | tee "/usercode/output.txt"
+				fi
+				
+				if diff -Z "/usercode/output.txt" "/usercode/test$i.out" > /dev/null
+				 then
+					echo "*-COMPILEBOX::DECISION-*,Accepted,test$i" >> "/usercode/logfile.txt"
+				 else
+					echo "*-COMPILEBOX::DECISION-*,Wrong Answer,test$i" >> "/usercode/logfile.txt"
+					break
+				fi
 
+			done    
+		#Branch 2b
+		else
+			echo "Compilation Failed" >> "usercode/logfile.txt"
+			#if compilation fails, display the output file	
+			#cat /usercode/errors.txt
+		fi
+	fi	
+else 
+  echo "type $type Working on judge, check back later :-)" >> "/usercode/logfile.txt"
+fi
 #exec 1>&3 2>&4
 
 #head -100 /usercode/logfile.txt
@@ -93,5 +124,4 @@ echo "*-COMPILEBOX::ENDOFOUTPUT-*" $runtime
 
 
 mv /usercode/logfile.txt /usercode/completed
-
 
